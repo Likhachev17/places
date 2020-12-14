356804c5323bb2b145de8d5d543d9471118cb2c9
Yung Yanni <mighty17wr@gmail.com>
2020-12-15 00:10:57 +0300

    -Удалены комментарии из pubspec.yaml, созданные при генерации проекта -Подключена библиотека flutter_svg -Хранилищем ассетов была установлена папка res -Добавлен шрифт Roboto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -97,9 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
+            Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
