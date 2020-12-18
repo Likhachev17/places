05539ae99a0d0b07fcaf865b7153b6f64e9586ed
Yung Yanni <mighty17wr@gmail.com>
2020-12-18 23:56:56 +0300

    1. Переименовал main.dart => start.dart и на первый взгляд ничего не изменилось Launching lib\start.dart on GM1910 in debug mode... Running Gradle task 'assembleDebug'... Хотя судя по видео-уроку, приложение не должно было запуститься, т.к. main.dart и void main() является точкой входа в программу, верно?
    4. Не смог найти отображение поля title, подскажете?
    
    5. Stateless widget не имеет ссылки на контекст
    
    6. Statefull widget имеет state, который имеет ссылку на контекст

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '3.5 homework',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -19,30 +19,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-class MyFirstWidget extends StatefulWidget {
-  @override
-  _MyFirstWidgetState createState() => _MyFirstWidgetState();
-}
-
-class _MyFirstWidgetState extends State<MyFirstWidget> {
+class MyFirstWidget extends StatelessWidget {
   int buildCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     buildCounter++;
-    Container(
+    print('количество вызовов build() $buildCounter раз');
+    return Container(
       child: Center(
         child: Text('Hello!'),
       ),
     );
-    print('количество вызовов build() $buildCounter раз');
-    return Container();
   }
 }
 
+class MySecondWidget extends StatefulWidget {
+  @override
+  _MySecondWidgetState createState() => _MySecondWidgetState();
+}
 
+class _MySecondWidgetState extends State<MySecondWidget> {
+  int buildCounter = 0;
 
+  Type getContextType() {
+    return context.runtimeType;
+  }
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    print(buildCounter++);
+    return Container(
+      child: Center(
+        child: Text('Hello!'),
+      ),
+    );
+  }
+}
