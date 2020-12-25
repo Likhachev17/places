8dce7b1314af6678176cd9910038202b7d0771f6
Yung Yanni <mighty17wr@gmail.com>
2020-12-25 22:16:17 +0300

    - Был создан файл sight.dart и папка domain в папке lib - Был создан файл mocks.dart в папке lib - Была создана подборка "интересных мест)))"

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  final String nameSight;
+  final double lan;
+  final double lon;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight(this.nameSight, this.lan, this.lon, this.url, this.details, this.type);
+}
---
+++ lib/mocks.dart
@@ -0,0 +1,20 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+      'Surf Studio',
+      51.664264,
+      39.201271,
+      'https://wampi.ru/image/RD1jDow',
+      'офис Surf Studio. Нормальной фотки не нашел, поэтому вот вам я. В камеру смотрю',
+      null),
+  Sight('Мой дом', 51.700504, 39.204497, 'https://wampi.ru/image/RD1Nlkc',
+      'Мой дом. Здесь я сижу и смотрю вас', null),
+  Sight(
+      'Блошиный рынок',
+      51.705625,
+      39.137255,
+      'https://wampi.ru/image/RD1QYM0',
+      'Блошиный рынок. Довольно странное место, но здесь я провожу все выходные покупая ерунду',
+      null)
+];
